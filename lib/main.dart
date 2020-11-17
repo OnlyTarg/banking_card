b84f8716c31e41964ad45f262241b7f13cd7564b
onlytarg <onlytarg@example.com>
2020-11-17 18:20:48 +0200

    start code

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:banking_card/screens/main_screen.dart';
+import 'package:banking_card/widgets/test.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      //home: MainScreen(),
+      home: Test(),
+    );
+  }
+}
+
